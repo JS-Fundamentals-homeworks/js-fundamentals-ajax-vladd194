2fa48da207d0de36c6cdafab4c1d905336150f53
vladd194 <vladtsvihun@gmail.com>
2024-07-23 13:04:40 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -5,3 +5,24 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const userNameInput = document.querySelector('#userNameInput');
+    const getUserButton = document.querySelector('#getUserButton');
+    const userCity = document.querySelector('#userCity');
+
+    getUserButton.addEventListener('click', () => {
+      const enteredName = userNameInput.value;
+      const user = data.find(u => u.name === enteredName);
+
+      if (user) {
+        userCity.textContent = user.address.city;
+      } else {
+        userCity.textContent = 'Користувача з таким іменем не знайдено';
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Помилка при отриманні даних:', error);
+  });
